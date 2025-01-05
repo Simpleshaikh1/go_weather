223c4152af0b3645b21dbd562718268b5a039b87
Toyyib <hamzatbolaji648@yahoo.com>
2025-01-05 18:12:04 +0100

    progress

--- main.go
+++ main.go
@@ -35,5 +35,22 @@ func loadApiConfig(filename string) (apiConfigData, error) {
 }
 
 func main() {
+	http.HandleFunc("/hello", hello)
 
+	http.HandleFunc("/weather/",
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := query(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(data)
+
+		}
+	)
+
+	http.ListenAndServe(":8083", nil)
 }
